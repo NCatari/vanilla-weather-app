b63653dde06c403ecfcbec21379ef7fcbde5b3b6
NCatari <117125032+NCatari@users.noreply.github.com>
2022-12-22 21:01:54 +0000

    created wind and humidity functions

--- src/script.js
+++ src/script.js
@@ -13,9 +13,22 @@ function showCondition(response) {
   conditionElement.innerHTML = response.data.weather[0].description;
 }
 
+function getHumidity(response) {
+  let humidityElement = document.querySelector("#humidity");
+  humidityElement.innerHTML = response.data.main.humidity;
+}
+
+function getWindspeed(response) {
+  console.log(response.data);
+  let windElement = document.querySelector("#wind");
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+}
+
 let apiKey = "1d038ee28ef2727a9f0310860ac10ae9";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=London&appid=${apiKey}&units=metric`;
 
 axios.get(apiUrl).then(showCity);
 axios.get(apiUrl).then(showTemperature);
 axios.get(apiUrl).then(showCondition);
+axios.get(apiUrl).then(getHumidity);
+axios.get(apiUrl).then(getWindspeed);
