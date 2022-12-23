bef02a2fa0a34534ecee98dbbdfd0d7066848b37
NCatari <117125032+NCatari@users.noreply.github.com>
2022-12-23 00:22:33 +0000

    created form and added search engine

--- src/script.js
+++ src/script.js
@@ -44,8 +44,19 @@ function showTemperature(response) {
   );
 }
 
-let apiKey = "1d038ee28ef2727a9f0310860ac10ae9";
-let city = "London";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+function search(city) {
+  let apiKey = "1d038ee28ef2727a9f0310860ac10ae9";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(showTemperature);
+}
 
-axios.get(apiUrl).then(showTemperature);
+function handleSubmit(event) {
+  event.preventDefault();
+  let cityInputElement = document.querySelector("#city-input");
+  search(cityInputElement.value);
+}
+
+search("London");
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
